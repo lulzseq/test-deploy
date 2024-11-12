5e3442c8537f081d1b5256623d1a7b6b221915bb
Dmitry <teoahoo@gmail.com>
2024-11-13 00:28:16 +0800

    c3

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 export const handleClick = async () => {
-    const response = await fetch('http://127.0.0.1:8000/', {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
